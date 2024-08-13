158e98e4ce107df378172e15100fb836946a5e12
root <root@ip-172-31-41-22.eu-north-1.compute.internal>
2024-08-13 09:02:15 +0000

    added numbers.java

---
+++ numbers.java
@@ -0,0 +1,15 @@
+public class LoginUser
+{
+        public static void main(String args[])
+        {
+                int i;
+
+
+                for(i=0;i<=55;i++){
+
+                
+
+                        System.out.println("Number: " + i);
+                }
+        }
+}
